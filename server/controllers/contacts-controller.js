cf1f783d29efd8064f85e498ce3d0397da6b01d0
arib7701 <amandineribot01@gmail.com>
2019-08-11 11:17:17 +0200

    update variables

--- server/controllers/contacts-controller.js
+++ server/controllers/contacts-controller.js
@@ -1,49 +1,41 @@
-var nodemailer        = require('nodemailer');
+var nodemailer = require('nodemailer');
 var smtpTransport = require('nodemailer-smtp-transport');
+var dotenv = require('dotenv').config();
 
-const user_name     = 'aribotprojects@gmail.com';
-const refresh_token = '1/CdWoZAbACohUGzOfYe5U_FqOtpoEyNfRdoZG06sMcFc';
-const access_token  = 'ya29.Gls5BUbZzxLB26eX-SfMILtAceGs3K5K8jb6UPtwvZnodw3znKTt0HVHHadqGJDnZt-apQBQvB0VFkknr2uVSsnXg6yEv9eIQx5fifVABqeXmg6Jkya53AhANqW7';
-const client_id     = '852079723483-bq4nlmo591l5ftrebiargo8t434ul7k3.apps.googleusercontent.com';
-const client_secret = 'I2NmxFd_EW-5L-g5-Uz0JbIE';
+var transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    type: 'OAuth2',
+    clientId: dotenv.CLIENT_ID,
+    clientSecret: dotenv.CLIENT_SECRET
+  }
+});
 
-var transporter = nodemailer
-    .createTransport({
-        service: 'Gmail',
-        auth: {
-            type: 'OAuth2',
-            clientId: client_id,
-            clientSecret: client_secret
-        }
-    });
+module.exports.sendEmail = function(req, res) {
+  console.log('into sendEmail - server side');
 
-module.exports.sendEmail = function (req, res) {
+  var data = req.body;
 
-    console.log("into sendEmail - server side");
+  var mailOptions = {
+    from: '"From "' + data.email, // sender address
+    to: 'aribotprojects@gmail.com', // list of receivers
+    subject: 'Message from ' + data.firstName + ' ' + data.lastName, // Subject line
+    text: data.tel + ' ' + data.mess, // plaintext body
 
-     var data = req.body;
+    auth: {
+      user: dotenv.USER_NAME,
+      refreshToken: dotenv.REFRESH_TOKEN,
+      accessToken: dotenv.ACCESS_TOKEN,
+      expires: 1494388182480
+    }
+  };
 
-     var mailOptions = {
-          from: '"From "' + data.email, // sender address
-          to: 'aribotprojects@gmail.com', // list of receivers
-          subject: 'Message from ' + data.firstName + ' ' + data.lastName, // Subject line
-          text: data.tel + ' ' + data.mess, // plaintext body
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message sent: ' + info.response);
+  });
 
-          auth : {
-            user         : user_name,
-            refreshToken : refresh_token,
-            accessToken  : access_token,
-            expires      : 1494388182480
-          }
-      };
- 
-    transporter.sendMail(mailOptions, function(error, info){
-        if(error){
-            return console.log(error);
-        }
-        console.log('Message sent: ' + info.response);
-    });
- 
-    res.json(data);
-
-}
+  res.json(data);
+};
